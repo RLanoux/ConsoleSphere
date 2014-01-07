5dcfabae170110a1f86583d1303723726facfa8e
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:30:16 -0600

    initial add

---
+++ ConsoleSphere.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initialize variables
+            Double dRadius = 0.0;
+            Double dVolume = 0.0;
+            String strUserResponse = String.Empty;
+
+            //Obtain the radius of the sphere
+            Console.Write("Enter the radius of the sphere: ");
+            strUserResponse = Console.ReadLine();
+            dRadius = Convert.ToDouble(strUserResponse);
+
+            //Calculate volume
+            dVolume = (4.0/3.0) * Math.PI * (dRadius * dRadius * dRadius);
+
+            //Inform user of the outcome
+            Console.WriteLine("Volume: " + dVolume.ToString());
+            Console.ReadLine();
+        }
+    }
+}
